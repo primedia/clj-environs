d1fbd874b9d642e54ca75b5b9ddd2e9e0f8eebc4
Jack Morrill <Jack@JackMorrill.com>
2013-07-16 07:31:26 -0600

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject environs "0.1.0"
-  :description "Retrive System Properties values loaded from the project .env file by dotenv"
+  :description "Retrive System Property values loaded from the project .env file by dotenv.core/load-env"
   :url "https://github.com/jackmorrill/environs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
