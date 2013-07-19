997ee90c77a05b7b88dc5ac55ef00cc00ff00b48
Jack Morrill <Jack@JackMorrill.com>
2013-07-19 07:18:07 -0600

    Add MIT license file

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject environs "0.1.0"
   :description "Retrive System Property values loaded from the project .env file by dotenv.core/load-env"
   :url "https://github.com/primedia/environs"
+  :license {:name "MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
