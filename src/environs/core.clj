a0af9244418c36c5b54a0ab93f0a535ed6881f6f
Jack Morrill <Jack@JackMorrill.com>
2013-09-19 08:39:39 -0600

    Updated metadata

--- src/environs/core.clj
+++ src/environs/core.clj
@@ -1,11 +1,18 @@
 (ns ^{:author "Jack Morrill"
-     :doc "Retrieve JVM System Properties that were loaded from the project .env file by dotenv."}
+     :doc "Retrieve JVM System Properties that were loaded from the project .env<.environment> file by clj-dotenv.
+
+     TODO
+
+     The environs function is supposed to take an optional parameter that tells environs to return nil instead of throwing an Exception
+
+     "}
+
      environs.core
      (:require [clojure.string :as string]))
 
 (defn get-env
   "Retrive System Property value.
-   If System Property is not found, attempt retrieve the Environment Variable withthe same neme.
+   If System Property is not found, attempt to retrieve the Environment Variable with the same neme.
    If both lookup fail, throw an exception"
   [env-name]
   (let [env-val (or (System/getProperty env-name) (System/getenv env-name))]
