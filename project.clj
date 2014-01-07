0f6dfe8ccafa100f3092c5e40d952bb31a6284c4
Jack Morrill <Jack@JackMorrill.com>
2014-01-07 07:28:17 -0700

    [JM/JB][00000000] Update load_repository

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject environs "1.0.1"
-  :description "Retrive System Property values loaded from the project .env file by dotenv.core/load-env"
-  :url "https://github.com/primedia/environs"
+  :description "Retrive System Property values loaded from the project .env file by com.rentpath.dotenv.core/dotenv!"
+  :url "https://github.com/primedia/clj-environs"
   :license {:name "MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
