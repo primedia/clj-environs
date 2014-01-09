2e9d7c50915b434523ba61ada18ec53cabcac64f
eric caspary <caspary.eric@gmail.com>
2014-01-09 16:07:36 -0500

    [EC] [000000] whitespace

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/primedia/environs"
   :license {:name "MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[ org.clojure/clojure "1.5.1" ]
-                 [ com.rentpath/dotenv "1.0.2" ]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.rentpath/dotenv "1.0.2"]]
   ;;:repositories {"project" "file:repo"})
   :repositories [["iws_pair" "http://ec2-54-224-24-95.compute-1.amazonaws.com/maven/.m2"]])
